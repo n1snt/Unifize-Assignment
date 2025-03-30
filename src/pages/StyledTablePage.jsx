4afca283ea580260009253f2ea8ec63764861065
Nishant <nishantbhandari0019@gmail.com>
2025-03-30 23:39:58 +0530

    Implemented styled table.

--- src/pages/StyledTablePage.jsx
+++ src/pages/StyledTablePage.jsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router";
+import MockTableData from "../mock/MockTableData";
+import MercuryTableStyled from "../components/MercuryTableStyled";
 
 const StyledTablePage = () => {
 
@@ -7,7 +9,8 @@ const StyledTablePage = () => {
     return (
         <>
             <h1>Styled Table</h1>
-            <p>(Write here)</p>
+            <p>(Using tailwind css)</p>
+            <MercuryTableStyled data={MockTableData} />
             <div className="card">
                 <button onClick={() => navigate(-1)}>
                     Back
